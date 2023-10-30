f5c526c6819c7ae7d8f88fc0ea9bccd276484602
Veikka <Veikka.puolitaival@gmail.com>
2023-10-30 23:07:02 +0200

    REST

---
+++ server/controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/user');
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+const pgp = require('pg-promise')();
+const config = require('../config');
+const db = pgp(config.databaseURL);
+
+const User = {
+  findAll: async () => {
+    return await db.any('SELECT * FROM users;');
+  }
+};
+
+module.exports = User;
