2a8ac3b26ea73f95581b9f3201245d33d2207b00
Veikka <Veikka.puolitaival@gmail.com>
2023-10-30 14:45:40 +0200

    Routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Start from './pages/Start/Start';
-
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route exact path="/" element={Start} />
-                    <Route path="/login" element={Login} />
-                    <Route path="/home" element={Home} />
+                    <Route path="/" element={<Start />} />
+                    <Route path="/login" element={<Login />} /> 
+                    <Route path="/home" element={<Home />} /> 
                 </Routes>
             </div>
         </Router>
--- client/src/pages/Start/Start.js
+++ client/src/pages/Start/Start.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Start() {
     return (
         <div>
             <h1>Elokuvasovellus Startpage</h1>
-
-
+            <Link to="/login">
+                <button>Login</button>
+            </Link>
         </div>
     );
 }
