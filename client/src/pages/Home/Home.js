094935e495fe7582faaf1b1ec68727cdd36ac652
Veikka <Veikka.puolitaival@gmail.com>
2023-10-30 23:19:08 +0200

    Client working

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import axios from 'axios';
 
 
 function HomePage() {
+    const handleButtonClick = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/api/users');
+            console.log(response.data);
+        }   catch (error) {
+            console.log(error);
+        }
+    };
     return (
         <div>
             <h1>Elokuvasovellus home</h1>
+            <button onClick={handleButtonClick}>Get Users</button>
         </div>
     );
 }
