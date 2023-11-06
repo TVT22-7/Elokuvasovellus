986cf07a4c1eacc467ace42ed0b7815cbece6343
OlliSaukko <t1saol00@students.oamk.fi>
2023-11-06 09:45:15 +0200

    createaccount
    create username, create password

--- client/src/pages/CreateAccount/CreateAccount.js
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -6,6 +6,21 @@ function CreateAccount() {
         <div>
             <h1>Elokuvasovellus CreateAccount</h1>
 
+            <div>
+                <label htmlFor=" create username"> Create Username:</label>
+                <input type="text" id="username" name="username" />
+            </div>
+
+            <div>
+                <label htmlFor="password">Create Password:</label>
+                <input type="password" id="password" name="password" />
+            </div>
+
+            <div>
+                <label htmlFor="password"> Repeat Password:</label>
+                <input type="password" id="password" name="password" />
+            </div>
+
             <Link to="/home">
                 <button>Home</button>
             </Link>
