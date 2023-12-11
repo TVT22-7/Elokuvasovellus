c9564edbe3f22460c545b154d9eb98a17adb7a0f
suopoika <henrik.junttila@gmail.com>
2023-12-11 13:59:10 +0200

    delete user
    käyttäjän poisto onnistuu, vanhoja käyttäjiä ei voi poistaa keksin takia

--- client/src/pages/settings/Settings.js
+++ client/src/pages/settings/Settings.js
@@ -1,31 +1,66 @@
-import React from 'react';
-import Navigation from '../../components/Navigation/Navigation';  // Import the Navigation component
+// Settings.js
+import React, { useState } from 'react';
+import Navigation from '../../components/Navigation/Navigation';
 import './Settings.css';
-import { useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 function Settings() {
-  
+  const [cookies, , removeCookie] = useCookies(['AuthToken', 'userId']);
+  const [isAccountDeleted, setIsAccountDeleted] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleDeleteAccount = async () => {
+    try {
+      // Check if the user is authenticated
+      if (!cookies.AuthToken || !cookies.userId) {
+        setIsAccountDeleted(false);
+        return;
+      }
 
+      const response = await fetch(
+        `${process.env.REACT_APP_ADDRESS}/api/users/${cookies.userId}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${cookies.AuthToken}`,
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
 
-  const handleDeleteAccount = () => {
-    // Code to handle deleting the account
+      if (response.ok) {
+        // Account deleted successfully
+        removeCookie('Username');
+        removeCookie('AuthToken');
+        removeCookie('userId');
+        setIsAccountDeleted(true); // Aseta tila, että tili on poistettu
+      } else {
+        setIsAccountDeleted(false);
+      }
+    } catch (error) {
+      console.error('Error deleting account', error);
+      setIsAccountDeleted(false);
+    }
   };
-
 
   return (
     <div className='asetukset'>
       <Navigation />
       <h1>Settings</h1>
-      
-      <button className='Delete-Acc' onClick={handleDeleteAccount}>Delete Account</button>
-      
-      
+
+      {isAccountDeleted ? (
+        <p>Account deleted</p>
+      ) : (
+        <div>
+          <button className='Delete-Acc' onClick={handleDeleteAccount}>
+            Delete Account
+          </button>
+        </div>
+      )}
     </div>
   );
 }
 
-
-
 export default Settings;
-
